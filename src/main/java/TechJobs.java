ef07bfdfcd16d91388d3f9111d03cc559a02591e
addie1202 <adlai.gaitherganim@gmail.com>
2023-08-26 17:50:48 -0500

    now its finished

--- src/main/java/TechJobs.java
+++ src/main/java/TechJobs.java
@@ -119,7 +119,6 @@ public class TechJobs {
 
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
-        System.out.println(someJobs.size());
         if (someJobs == null || someJobs.size() == 0) {
             System.out.println("No Results");
             return;
